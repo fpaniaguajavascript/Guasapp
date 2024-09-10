bff263b64a64bdc655fa6bd4ec5462e5958db17c
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-09-10 21:00:08 +0200

    Lectura de cambios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,17 +2,24 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonItem, IonList } from '@ionic/angular/standalone';
 import { FirebaseRTBService } from '../services/firebase-rtb.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonList, IonItem, IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [FormsModule, CommonModule, IonList, IonItem, IonButton, IonInput, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   public mensajes: string[] = [];
+  public from: string = "";
+  public to: string = "";
+  public message: string = "";
   constructor(private firebaseService: FirebaseRTBService) {
     this.mensajes = this.firebaseService.mensajes;
+  }
+  public enviar() {
+    console.log("Enviando...");
   }
 }

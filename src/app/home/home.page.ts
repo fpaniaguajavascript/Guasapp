6c3218274306c6f86025b6fbde7a71d4f3bc5e65
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-09-11 19:08:13 +0200

    Mejoras en el registro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,8 @@ import { FirebaseRTBService } from '../services/firebase-rtb.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+const ALIAS_KEY = "ALIAS"
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -15,10 +17,21 @@ export class HomePage {
   public mensajes: any[] = [];
   public from: string = "";
   public message: string = "";
+  public registrado: boolean = false;
   constructor(public firebaseService: FirebaseRTBService) {
     this.mensajes = this.firebaseService.mensajes;
+    let alias = localStorage.getItem(ALIAS_KEY);
+    if (alias) {
+      this.registrado = true;
+      this.from = alias;
+    }
   }
   public enviar() {
     this.firebaseService.enviarMensaje(this.from, this.message);
+    this.message = "";
+  }
+  public registrarUsuario() {
+    localStorage.setItem(ALIAS_KEY, this.from)
+    this.registrado = true;
   }
 }
